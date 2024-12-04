9f8b12ad4551d8f86099a706b31aa0c0d20b9788
poltow <poltow@gmail.com>
2024-12-04 15:41:37 -0300

    Spring Initializr init

---
+++ src/main/java/com/conistock/securo/SecuroApplication.java
@@ -0,0 +1,13 @@
+package com.conistock.securo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuroApplication.class, args);
+	}
+
+}
