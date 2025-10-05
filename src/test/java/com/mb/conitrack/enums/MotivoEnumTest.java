76dec913df02bf9e72ba610cc7c4fc58be234f13
pablotorresdev <pablotorresdev@gmail.com>
2025-10-05 00:10:53 -0300

    Formated code, remove unused tests

--- src/test/java/com/mb/conitrack/enums/MotivoEnumTest.java
+++ src/test/java/com/mb/conitrack/enums/MotivoEnumTest.java
@@ -41,30 +41,4 @@ class MotivoEnumTest {
         assertThrows(IllegalArgumentException.class, call);
     }
 
-    @Test
-    @DisplayName("values() contiene exactamente las 12 constantes en orden")
-    void valuesContentAndOrder() {
-        MotivoEnum[] v = MotivoEnum.values();
-        assertEquals(14, v.length, "Cantidad de constantes");
-
-        assertEquals(MotivoEnum.COMPRA, v[0]);
-        assertEquals(MotivoEnum.MUESTREO, v[1]);
-        assertEquals(MotivoEnum.DEVOLUCION_COMPRA, v[2]);
-        assertEquals(MotivoEnum.ANALISIS, v[3]);
-        assertEquals(MotivoEnum.RESULTADO_ANALISIS, v[4]);
-        assertEquals(MotivoEnum.CONSUMO_PRODUCCION, v[5]);
-        assertEquals(MotivoEnum.PRODUCCION_PROPIA, v[6]);
-        assertEquals(MotivoEnum.LIBERACION, v[7]);
-        assertEquals(MotivoEnum.VENTA, v[8]);
-        assertEquals(MotivoEnum.EXPIRACION_ANALISIS, v[9]);
-        assertEquals(MotivoEnum.VENCIMIENTO, v[10]);
-        assertEquals(MotivoEnum.DEVOLUCION_VENTA, v[11]);
-        assertEquals(MotivoEnum.RETIRO_MERCADO, v[12]);
-        assertEquals(MotivoEnum.AJUSTE, v[13]);
-
-        Set<MotivoEnum> all = EnumSet.allOf(MotivoEnum.class);
-        assertEquals(all.size(), v.length);
-        assertTrue(all.containsAll(Set.of(v)));
-    }
-
 }
